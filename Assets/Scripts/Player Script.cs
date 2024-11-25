2d5b41f41e21667edb1c4dc684b8a4f0fa342ba5
OTWhite04 <owen.t.white2005@gmail.com>
2024-11-25 12:21:48 -0400

    Working on Jumping

--- Assets/Scripts/Player Script.cs
+++ Assets/Scripts/Player Script.cs
@@ -7,8 +7,11 @@ public class PlayerScript : MonoBehaviour
 {
     public Rigidbody2D rb;
     public float speed = 20;
+    public float jumpforce = 10f;
 
     private float inputX;
+
+  
 
     private void Update()
     {
@@ -20,7 +23,28 @@ public class PlayerScript : MonoBehaviour
         rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
     }
 
+    void Flip()
+    {
+        if (inputX > 0)
+        {
+            if (transform.eulerAngles.y != 0f)
+            {
+                transform.eulerAngles = new Vector3(0f, 0f, 0f);
+            }
+        }
+        else
+        {
+            if (transform.eulerAngles.y != 180f)
+            {
+                transform.eulerAngles = new Vector3(0f, 180f, 0f);
+            }
+        }
 
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpforce);
+        }
+    }
 
 
 
